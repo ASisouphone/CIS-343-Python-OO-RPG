944a4c37e036cc04813aab342b774645d077e03b
ASisouphone <sisouphone.alan@gmail.com>
2018-03-16 01:53:47 -0400

    Defined classes

---
+++ RPG.py
@@ -0,0 +1,36 @@
+class Home(Observer):
+    def __init__(self):
+
+class Neighborhood():
+    def __init__(self):
+
+
+class NPC(Object):
+    def __init__(self):
+
+class Persons(NPC):
+    def __init__(self):
+        super().__init__()
+
+class Zombies(NPC):
+    def __init__(self):
+        super().__init__()
+
+class Vampires(NPC):
+    def __init__(self):
+        super().__init__()
+
+class Ghouls(NPC):
+    def __init__(self):
+        super().__init__()
+
+class Werewolves(NPC):
+    def __init__(self):
+        super().__init__()
+
+class Weapon(Object):
+    def __init__(self):
+
+class Player(Object):
+
+class Game(Object):
